da8fdff68d0b95d2bb4666492aa2a0805372da41
Ramtin Abolfazli <Ramtina1381@gmail.com>
2024-01-31 12:28:08 -0500

    Fourth Lab

--- Labs/Lab1/Comp_2139/Comp_2139/Controllers/ProjectController.cs
+++ Labs/Lab1/Comp_2139/Comp_2139/Controllers/ProjectController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Comp_2139.Data;
 using Comp_2139.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,13 +13,18 @@ namespace Comp_2139.Controllers
 {
     public class ProjectController : Controller
     {
+
+        private readonly ApplicationDbContext context;
+        private ApplicationDbContext _context;
+
+        public ProjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            var projects = new List<Project>()
-            {
-                new Project { ProjectId = 1, Name = "Project 1", Description = "My First Proejct"}
-            };
+            var projects = _context.Projects.ToList();
             return View(projects);
         }
 
@@ -28,17 +35,89 @@ namespace Comp_2139.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryTokenAttribute]
         public IActionResult Create(Project project)
         {
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _context.Projects.Add(project);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(project);
         }
 
         [HttpGet]
         public IActionResult Details(int id)
         {
-            var project = new Project { ProjectId = id, Name = "Project " + id, Description = "Details of Project " + id };
+            var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+            return View(project);
+            if(project == null)
+            {
+                return NotFound();
+            }
+            return View(project);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var project = _context.Projects.Find(id);
+            if(project == null)
+            {
+                return NotFound();
+            }
+            return View(project);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("ProjectId, Name, Description")]Project project)
+        {
+            if(id != project.ProjectId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(project);
+                    _context.SaveChanges();
+                }
+                catch(DbUpdateConcurrencyException)
+                {
+                    if (!ProjectExists(project.ProjectId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(project);
+        }
+
+        public bool ProjectExists(int id)
+        {
+            return _context.Projects.Any(e => e.ProjectId == id);
+        }
+
+        [HttpGet]
+
+        public IActionResult Delete(int id)
+        {
+            var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+            if(project == null)
+            {
+                return NotFound();
+            }
             return View(project);
         }
     }
+
 }
 
